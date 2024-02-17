e577a5de9de12e2247c98d27db3adb8e25541c15
iwantanode <87604944+tudorpintea999@users.noreply.github.com>
2024-02-17 09:08:42 +0200

    Update run.sh

--- configs/optimism/run.sh
+++ configs/optimism/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd simple-optimism-node
+sed -i 's/:7300/:7303/g' docker-compose.yml
+sed -i 's/\${PORT__HEALTHCHECK_METRICS:-7300}/\${PORT__HEALTHCHECK_METRICS:-7303}/g' docker-compose.yml
 docker compose up -d --build
