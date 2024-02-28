ca8f24a9c6a7642a76cfb9c90c06fd741010b43a
Kyon <kyonosuke.eth@gmail.com>
2024-02-28 18:08:18 +0500

    path fix

--- app/node-installer.js
+++ app/node-installer.js
@@ -8,7 +8,7 @@ import ssh from "../utils/ssh.js"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-console.log(path.join(__dirname, "..", ".env"))
+// console.log(path.join(__dirname, "..", ".env"))
 
 dotenv.config({ path: path.join(__dirname, "..", ".env") })
 
--- utils/db.js
+++ utils/db.js
@@ -6,13 +6,13 @@ import pkg from "pg"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-console.log(path.join(__dirname, "..", ".env"))
+// console.log(path.join(__dirname, "..", ".env"))
 dotenv.config({ path: path.join(__dirname, "..", ".env") })
 
 const { Pool } = pkg
 
 const connectionString = process.env.SUPABASE
-console.log("connectionString:", connectionString)
+// console.log("connectionString:", connectionString)
 
 const database = new Pool({
   connectionString,
