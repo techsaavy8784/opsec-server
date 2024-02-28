5ea98e62f5a3415d6f6f75ff86fcc9d5fdbbf69f
Kyon <kyonosuke.eth@gmail.com>
2024-02-28 18:07:25 +0500

    path fix

--- utils/db.js
+++ utils/db.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 console.log(path.join(__dirname, "..", ".env"))
+dotenv.config({ path: path.join(__dirname, "..", ".env") })
 
 const { Pool } = pkg
 
