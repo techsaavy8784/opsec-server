e3429455af0d167f2e252b4642014ecd79ca0d84
iwantanode <87604944+tudorpintea999@users.noreply.github.com>
2024-03-04 22:15:30 +0200

    Update run.sh

--- configs/bevm/run.sh
+++ configs/bevm/run.sh
@@ -1,1 +1,2 @@
-sudo docker run -d -v /var/lib/node_bevm_test_storage:/root/.local/share/bevm btclayer2/bevm:v0.1.1 bevm "--chain=testnet" "--name=address" "--pruning=archive" --telemetry-url "wss://telemetry.bevm.io/submit 0"
+#!/bin/bash
+sudo docker run -d -v /var/lib/node_bevm_test_storage:/root/.local/share/bevm btclayer2/bevm:v0.1.1 bevm "--chain=testnet" "--name=$(cat .wallet)" "--pruning=archive" --telemetry-url "wss://telemetry.bevm.io/submit 0"
