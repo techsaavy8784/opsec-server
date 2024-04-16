ad3f4096b595db89baeea1a444e11546cec04be5
techsaavy8784 <blueskydev1205@gmail.com>
2024-04-16 19:45:46 +0900

    fix: write contract sync

--- app/web3.js
+++ app/web3.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 import { CovalentClient } from "@covalenthq/client-sdk";
 import { http, createPublicClient, createWalletClient, custom } from "viem"
-import { mainnet } from 'viem/chains'
+import { mainnet,sepolia } from 'viem/chains'
 import abi from "../utils/abi.json" assert { type: "json" }
 
 const restrict_check = async (address) => {
@@ -28,7 +28,7 @@ export const publicClient = createPublicClient({
 })
 
 export const walletClient = createWalletClient({
-  chain: mainnet,
+  chain: sepolia,
   // transport: custom(window.ethereum!)
   transport: http()
 })
@@ -36,6 +36,9 @@ export const walletClient = createWalletClient({
 const BLOCK_INTERVAL = 3
 
 const SAVE_FILE_PATH = path.join(process.env.HOME, ".opsec")
+const SAVE_FILE_PATH_CLAIM = path.join(process.env.HOME, ".opsec_claim")
+
+let claimIntervalID = null;
 
 const listenStake = async () => {
   let lastBlockNumber = 0
@@ -98,64 +101,132 @@ const listenStake = async () => {
   }, 5000)
 }
 
-const listenClaim = ()  => {
+const listenClaim = async () => {
+  let lastBlockNumber = 0
+
+  if (fs.existsSync(SAVE_FILE_PATH_CLAIM)) {
+    lastBlockNumber = Number(fs.readFileSync(SAVE_FILE_PATH_CLAIM))
+  }
+
+  if (lastBlockNumber === 0) {
+    lastBlockNumber = await publicClient.getBlockNumber()
+  }
+
+  console.log(`start: ${lastBlockNumber}`)
+
+  claimIntervalID = setInterval(async () => {
+    try {
+      const blockNumber = Number(await publicClient.getBlockNumber())
+
+      console.log(`block number: ${blockNumber}`)
+
+      if (blockNumber - BLOCK_INTERVAL < lastBlockNumber) {
+        console.log(`skipping ${blockNumber}`)
+        return
+      }
+
+      console.log(`block number: from ${lastBlockNumber} to ${blockNumber}`)
+
+      const filter = await publicClient.createContractEventFilter({
+        abi,
+        address: process.env.STAKE_CONTRACT,
+        eventName: "Claimed",
+        fromBlock: lastBlockNumber,
+        toBlock: blockNumber,
+      })
+
+      const logs = await publicClient.getFilterLogs({ filter })
+
+      for (const log of logs) {
+        const address = log.args.user
+
+        console.log(`Claim event: ${address}`)
+
+        await fetch(`${process.env.OPSEC_DAPP_URL}/api/claim/confirm`, {
+          body: JSON.stringify({ address: address }),
+          method: "POST",
+          headers: {
+            "X-API-KEY": process.env.STAKE_WEBHOOK_KEY,
+          },
+        })
+      }
+
+      lastBlockNumber = blockNumber + 1
+      console.log(`last block number: ${lastBlockNumber}`)
+      fs.writeFileSync(SAVE_FILE_PATH_CLAIM, String(lastBlockNumber))
+    } catch (e) {
+      console.error(`error: ${JSON.stringify(e)}`)
+    }
+  }, 5000)
+}
+
+const batchClaim = ()  => {
   let addressParam = []
   let amountParam = []
   let userIdParam = []
-  fetch(`${process.env.OPSEC_DAPP_URL}/api/claim/all`, {
-    body: JSON.stringify({ stakeId }),
+  if(claimIntervalID !== null) 
+    clearInterval(claimIntervalID)
+  fetch(`${process.env.OPSEC_DAPP_URL}/api/claim/confirm/check`, {
     method: "GET",
     headers: {
       "X-API-KEY": process.env.STAKE_WEBHOOK_KEY,
     },
-  }).then((res) => res.json())
-    .then(async (res) =>{
-      console.log(`claim datas: ${res.data}`)
+  }).then((res) => {
 
-      res.data.map(async (item, id) => {
-        userIdParam.push(item.user_id)
-        if(!restrict_check(item.address))
+    fetch(`${process.env.OPSEC_DAPP_URL}/api/claim/all`, {
+      method: "GET",
+      headers: {
+        "X-API-KEY": process.env.STAKE_WEBHOOK_KEY,
+      },
+    }).then((res) => res.json())
+      .then(async (res) =>{
+        console.log(`claim datas: ${res.data}`)
+  
+        res.data.map(async (item) => {
+          userIdParam.push(item.user_id)
+          if(!restrict_check(item.address))
+            return;
+          addressParam.push(item.address)
+          amountParam.push(item.amount)
           return;
-        addressParam.push(item.address)
-        amountParam.push(item.amount)
-        return;
-      })
-
-      try {
-
-        const { request } = await publicClient.simulateContract({
-          // account: walletClient.requestAddresses(),
-          account: process.env.OWER_ACCOUNT,
-          address: process.env.STAKE_CONTRACT,
-          abi,
-          functionName: 'claim',
-          args: [ 
-            addressParam,
-            amountParam
-          ]
         })
-
-        await walletClient.writeContract(request)
-
-        userIdParam.map(async (item) => {
-          await fetch(`${process.env.OPSEC_DAPP_URL}/api/claim/update`, {
-            body: JSON.stringify({ userId: item }),
-            method: "POST",
-            headers: {
-              "X-API-KEY": process.env.STAKE_WEBHOOK_KEY,
-            },
+  
+        try {
+  
+          const { request } = await publicClient.simulateContract({
+            // account: walletClient.requestAddresses(),
+            account: process.env.OWER_ACCOUNT,
+            address: process.env.STAKE_CONTRACT,
+            abi,
+            functionName: 'claim',
+            args: [ 
+              amountParam,
+              addressParam
+            ]
           })
-        })
-      } catch(e) {
-        console.log("error: ", e);
+  
+          await walletClient.writeContract(request)
+  
+          userIdParam.map(async (item) => {
+            await fetch(`${process.env.OPSEC_DAPP_URL}/api/claim/update`, {
+              body: JSON.stringify({ userId: item }),
+              method: "POST",
+              headers: {
+                "X-API-KEY": process.env.STAKE_WEBHOOK_KEY,
+              },
+            })
+          })
+          listenClaim()
+        } catch(e) {
+          console.log("error: ", e);
+        }
       }
-      
-    }
-  )
+    )
+  })
 }
 
-listenStake()
+// listenStake()
 
-// for test, period of cron job is 20s. for 24hr  '0 0 * * *'
-const job = nodeCron.schedule('*/20 * * * * *', listenClaim);
+// // for test, period of cron job is 20s '*/60 * * * * *' . for 24hr  '0 0 * * *'
+const job = nodeCron.schedule('0 0 * * *', batchClaim);
 job.start();
