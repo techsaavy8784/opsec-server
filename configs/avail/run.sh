4c1376a046cae49bfdf18d2ed1c212b4b9ce1989
iwantanode <87604944+tudorpintea999@users.noreply.github.com>
2024-04-22 11:30:39 +0300

    Update run.sh

--- configs/avail/run.sh
+++ configs/avail/run.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-sudo docker run \
-  -v "$(pwd)/state:/da/state:rw" \
-  -p 30333:30333 \
-  -p 9615:9615 \
-  -p 9949:9949 \
-  -d \
-  --restart unless-stopped \
-  --name avail-node \
-  availj/avail:v1.10.0.0 \
-  --chain goldberg \
-  --name "$(cat .wallet)" \
-  -d /da/state
+sudo docker run -v $(pwd)/state:/da/state:rw -p 30383:30383 -p 9615:9615 -p 9944:9944 -d --restart unless-stopped availj/avail:v1.10.0.0 --chain goldberg --name "$(cat .wallet)" -d /da/state
